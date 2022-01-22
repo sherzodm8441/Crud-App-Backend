c79fb37bbb0439c946ab335105566db6bb0623ec
Sherzod Mirsharipov <mirsharipovsherzod@gmail.com>
2022-01-22 14:55:53 -0500

    closes #3; add get request that gets student info with id including campus

--- api/students.js
+++ api/students.js
@@ -1,10 +1,23 @@
 const router = require('express').Router()
 const { Student } = require('../db')
+const { Campus } = require('../db')
 
 router.get('/', async (req, res) => {
     try{
         const students = await Student.findAll()
         res.status(200).send(students)
+    }catch(error){
+        res.status(404).send(error.message)
+    }
+})
+
+router.get('/:id', async (req, res) =>{
+    try{
+        const student = await Student.findByPk(req.params.id)
+        const temp = student.toJSON()
+        const campus = await Campus.findByPk(temp.campusId)
+        temp['campus'] = campus
+        res.status(200).send(temp)
     }catch(error){
         res.status(404).send(error.message)
     }
